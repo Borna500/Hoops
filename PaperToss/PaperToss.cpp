3fe784dfd5340e78f4799a8ef0e08c84197f9136
Neil Robichaud <neil.robichaud@hotmail.com>
2016-12-07 15:35:28 -0500

    walls of jericho

--- PaperToss/PaperToss.cpp
+++ PaperToss/PaperToss.cpp
@@ -16,6 +16,7 @@
 #include <stdlib.h>
 #include <ctime>
 #include <cmath>
+#include "math3D.h"
 
 int firstwindow;
 int secondwindow;
@@ -29,6 +30,8 @@ float shiny = 27.8;
 float yAngle = 0;
 float xAngle = 0;
 
+bool selected = false;
+bool throwing = false;
 bool launched = false;
 int startingMousepos[2];
 int finalMousepos[2];
@@ -38,18 +41,18 @@ float averageXVelocity;
 float averageZVelocity;
 
 float windspeed = 0;
-float startingPos[] = {0,1,-5};
-float position[] = {0,1,-5};
+float position[] = {0,1,-4};
+float startingPos[] = {0,1,-4};
 float velocity[]  = {0,0,0};
-float acceleration[] = {windspeed,-30,0};
+float acceleration[] = {windspeed,-100,0};
 
 /*** 
  EYE LOCATION
  ***/
 float eye[] = {0,1,-7};
 float lookat[] = {0,0,0};
-float light1_pos[]= {0,5,0,1}; //last num one is pofloat light
-float light2_pos[]= {300,5,300,1}; //last num one is pofloat light
+float light1_pos[]= {0,4,-4,1}; //last num one is pofloat light
+float light2_pos[]= {0,0,0,1}; //last num one is pofloat light
 
 
 float angle1 = 0.0f;
@@ -77,169 +80,8 @@ mapType currentMap;
 bool vertexShading;
 
 
-//face normal map
-float normalMap[1000][1000][3];
-//vertex normal map
-float vertexnormalMap[1000][1000][3];
-void calcNormals(){
-    //face normals calculated by taking the cross product of two perpendicular vectors on the face
-    for (int x=0; x< mapSize; x++ ){
-        for(int z = 0; z < mapSize; z++){
-            float point1[] = {static_cast<float>(x),heightMap[x][z],static_cast<float>(z)};
-            float point2[] = {static_cast<float>(x),heightMap[x][z+1],static_cast<float>(z+1)};
-            float point3[] = {static_cast<float>(x+1),heightMap[x+1][z],static_cast<float>(z)};
-            
-            float v[3];
-            v[0] = point2[0] - point1[0];
-            v[1] = point2[1] - point1[1];
-            v[2] = point2[2] - point1[2];
-            float u[3];
-            u[0] = point3[0] - point1[0];
-            u[1] = point3[1] - point1[1];
-            u[2] = point3[2] - point1[2];
-            
-            
-            
-            float s1 = u[1]*v[2] - u[2]*v[2];
-            float s2 = u[2]*v[0] - v[2]*u[0];
-            float s3 = u[0]*v[1] - v[0]*u[1];
-            
-            float length = sqrtf(s1*s1 + s2*s2 + s3*s3);
-            normalMap[x][z][0] = s1/length;
-            normalMap[x][z][1] = s2/length;
-            normalMap[x][z][2] = s3/length;
-            
-        }
-        
-    }
-    //vertex normal is calculated by normalizing and averaging all the face normals surrounding a given vertex
-    for (int x=0; x< mapSize; x++ ){
-        for(int z = 0; z < mapSize; z++){
-            if (0 < x && x < mapSize && z > 0 && z < mapSize){
-                vertexnormalMap[x][z][0] = (normalMap[x][z+1][0] + normalMap[x][z-1][0] + normalMap[x+1][z][0] + normalMap[x-1][z][0])/4;
-                vertexnormalMap[x][z][1] = (normalMap[x][z+1][1] + normalMap[x][z-1][1] + normalMap[x+1][z][1] + normalMap[x-1][z][1])/4;
-                vertexnormalMap[x][z][2] = (normalMap[x][z+1][2] + normalMap[x][z-1][2] + normalMap[x+1][z][2] + normalMap[x-1][z][2])/4;
-            }
-        }}
-    
-    
-}
-
 float minHeight = 0;
 float maxHeight = 0;
-
-
-//when middle of ball hits y of 0.5 check whether x and z are in basket bounds
-
-//draws the mesh as quads
-
-//same as quads, but just with triangles
-void drawTriangles(){
-    for(int x = 0; x< maxX; x++){
-        for (int z = 0; z < maxZ; z++){
-            if (!vertexShading){
-            glBegin(GL_TRIANGLES);
-            
-            glColor3f((heightMap[x][z+1] - minHeight)/(maxHeight - minHeight),(heightMap[x][z+1] - minHeight)/(maxHeight - minHeight),(heightMap[x][z+1] - minHeight)/(maxHeight - minHeight));
-            glNormal3fv(normalMap[x][z+1]);
-            glVertex3f(x,heightMap[x][z+1],z+1);
-            
-            glColor3f((heightMap[x][z] - minHeight)/(maxHeight - minHeight),(heightMap[x][z] - minHeight)/(maxHeight - minHeight),(heightMap[x][z] - minHeight)/(maxHeight - minHeight));
-            glNormal3fv(normalMap[x][z]);
-            glVertex3f(x,heightMap[x][z], z);
-            
-            glColor3f((heightMap[x+1][z] - minHeight)/(maxHeight - minHeight),(heightMap[x+1][z] - minHeight)/(maxHeight - minHeight),(heightMap[x+1][z] - minHeight)/(maxHeight - minHeight));
-            glNormal3fv(normalMap[x+1][z]);
-            glVertex3f(x+1, heightMap[x+1][z], z);
-            glEnd();
-            
-            glBegin(GL_TRIANGLES);
-            
-            glColor3f((heightMap[x+1][z] - minHeight)/(maxHeight - minHeight),(heightMap[x+1][z] - minHeight)/(maxHeight - minHeight),(heightMap[x+1][z] - minHeight)/(maxHeight - minHeight));
-            glNormal3fv(normalMap[x+1][z]);
-            glVertex3f(x+1,heightMap[x+1][z],z);
-            
-            glColor3f((heightMap[x+1][z+1] - minHeight)/(maxHeight - minHeight),(heightMap[x+1][z+1] - minHeight)/(maxHeight - minHeight),(heightMap[x+1][z+1] - minHeight)/(maxHeight - minHeight));
-            glNormal3fv(normalMap[x+1][z+1]);
-            glVertex3f(x+1,heightMap[x+1][z+1], z+1);
-            
-            glColor3f((heightMap[x][z+1] - minHeight)/(maxHeight - minHeight),(heightMap[x][z+1] - minHeight)/(maxHeight - minHeight),(heightMap[x][z+1] - minHeight)/(maxHeight - minHeight));
-            glNormal3fv(normalMap[x][z+1]);
-            glVertex3f(x, heightMap[x][z+1], z+1);
-            glEnd();
-            }
-            else{
-                glBegin(GL_TRIANGLES);
-                
-                glColor3f((heightMap[x][z+1] - minHeight)/(maxHeight - minHeight),(heightMap[x][z+1] - minHeight)/(maxHeight - minHeight),(heightMap[x][z+1] - minHeight)/(maxHeight - minHeight));
-                glNormal3fv(vertexnormalMap[x][z+1]);
-                glVertex3f(x,heightMap[x][z+1],z+1);
-                
-                glColor3f((heightMap[x][z] - minHeight)/(maxHeight - minHeight),(heightMap[x][z] - minHeight)/(maxHeight - minHeight),(heightMap[x][z] - minHeight)/(maxHeight - minHeight));
-                glNormal3fv(vertexnormalMap[x][z]);
-                glVertex3f(x,heightMap[x][z], z);
-                
-                glColor3f((heightMap[x+1][z] - minHeight)/(maxHeight - minHeight),(heightMap[x+1][z] - minHeight)/(maxHeight - minHeight),(heightMap[x+1][z] - minHeight)/(maxHeight - minHeight));
-                glNormal3fv(vertexnormalMap[x+1][z]);
-                glVertex3f(x+1, heightMap[x+1][z], z);
-                glEnd();
-                
-                glBegin(GL_TRIANGLES);
-                
-                glColor3f((heightMap[x+1][z] - minHeight)/(maxHeight - minHeight),(heightMap[x+1][z] - minHeight)/(maxHeight - minHeight),(heightMap[x+1][z] - minHeight)/(maxHeight - minHeight));
-                glNormal3fv(vertexnormalMap[x+1][z]);
-                glVertex3f(x+1,heightMap[x+1][z],z);
-                
-                glColor3f((heightMap[x+1][z+1] - minHeight)/(maxHeight - minHeight),(heightMap[x+1][z+1] - minHeight)/(maxHeight - minHeight),(heightMap[x+1][z+1] - minHeight)/(maxHeight - minHeight));
-                glNormal3fv(vertexnormalMap[x+1][z+1]);
-                glVertex3f(x+1,heightMap[x+1][z+1], z+1);
-                
-                glColor3f((heightMap[x][z+1] - minHeight)/(maxHeight - minHeight),(heightMap[x][z+1] - minHeight)/(maxHeight - minHeight),(heightMap[x][z+1] - minHeight)/(maxHeight - minHeight));
-                glNormal3fv(vertexnormalMap[x][z+1]);
-                glVertex3f(x, heightMap[x][z+1], z+1);
-                glEnd();
-            }
-            
-        }
-    }
-}
-//function to deal with all the toggle options and call the appropriate drawing func
-void drawHeightMap(){
-    glPushMatrix();
-    //move and rotate the map according to the user's commands, alternative to moving the camera around
-    glTranslatef(-mapSize,0,-mapSize);
-    glRotatef(xAngle, 1, 0, 0);
-    glRotatef(yAngle, 0, 1, 0);
-    switch (currentMode) {
-        case solid:
-             glPolygonMode(GL_FRONT_AND_BACK,GL_FILL);
-            break;
-        case wire:
-             glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
-            break;
-        case both:
-            glPolygonMode(GL_FRONT_AND_BACK,GL_FILL);
-            if (triangles)drawTriangles();
-            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-            break;
-    }
-    switch ( currentShading) {
-        case smooth:
-            glShadeModel(GL_SMOOTH);
-            break;
-        case flat:
-            glShadeModel(GL_FLAT);
-            break;
-    }
-    if (lighting){
-        glEnable(GL_LIGHTING);
-    }
-    else{
-        glDisable(GL_LIGHTING);
-    }
-    
-    if(triangles)drawTriangles();
-}
 
 
 void drawFloor(){
@@ -252,18 +94,51 @@ void drawFloor(){
         glVertex3f(-5, 0, -5);
         glVertex3f(-5, 0, 5);
         glEnd();
-    
-        glPushMatrix();
-            glColor3f(1, 1, 1);
-                glPushMatrix();
-                glTranslatef(position[0],position[1],position[2]);
-    
-                    //printf("%f",position[1]);
-                glutSolidSphere(1, 100, 100);
-            glPopMatrix();
-        glPopMatrix();
     glPopMatrix();
+    
+    glPushMatrix();
+    glTranslatef(0, 10, 0);
+    glBegin(GL_QUADS);
+    glVertex3f(5, 0, 5);
+    glVertex3f(5, 0, -5);
+    glVertex3f(-5, 0, -5);
+    glVertex3f(-5, 0, 5);
+    glEnd();
+    glPopMatrix();
+    
+    glPushMatrix();
+    glTranslatef(-5, 5, 0);
+    glRotatef(90, 0, 0, 1);
+    glBegin(GL_QUADS);
+    glVertex3f(5, 0, 5);
+    glVertex3f(5, 0, -5);
+    glVertex3f(-5, 0, -5);
+    glVertex3f(-5, 0, 5);
+    glEnd();
+    glPopMatrix();
+    
+    glPushMatrix();
+    glTranslatef(-5, 5, 0);
+    glRotatef(90, 0, 0, 1);
+    glBegin(GL_QUADS);
+    glVertex3f(5, 0, 5);
+    glVertex3f(5, 0, -5);
+    glVertex3f(-5, 0, -5);
+    glVertex3f(-5, 0, 5);
+    glEnd();
+    glPopMatrix();
+    
 
+
+}
+
+void drawBall(){
+    glColor3f(1, 1, 1);
+    glPushMatrix();
+    glTranslatef(position[0],position[1],position[2]);
+    //glutSolidSphere(1, 100, 100);
+    glutSolidCube(1);
+    glPopMatrix();
 }
 bool grounded = true;
 void ballMotion(int value){
@@ -360,34 +235,24 @@ void special(int key, int x, int y)
 }
 
 void init(void)
-{
-    printf("commands:\n 'n' = toggle shading\n 't' = triangles/quads\n 'u' = solid/wire/both\n 'l' = lighting\n 'y' = toggle size 50/300\n 'f' = toggle fault/circles algorithm\n 'v' = toggle vertex/face normals\n 'r' = draw new map\n 'wasd' = move lights\n extra features: fault algorithm, 2D representation, manual terrain manipulation by clicking on map");
-	glClearColor(0, 0, 0, 0);
+{	glClearColor(0, 0, 0, 0);
 	glColor3f(1,0,0);
     
-//    glEnable(GL_LIGHTING);
-//    glEnable(GL_LIGHT0);
-//    glLightfv(GL_LIGHT0, GL_AMBIENT, amb0);
-//	glLightfv(GL_LIGHT0, GL_DIFFUSE, diff0);
-//    glLightfv(GL_LIGHT0, GL_SPECULAR, spec0);
-    /************************************************************************
-     
-                            PERSPECTIVE SET UP!
-     
-     modelview moves the eye and objects, projection is for camera type
-     frustum / ortho left, right, bottom, top, nearVal, farVal
-     
-     ************************************************************************/
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    glEnable(GL_LIGHT1);
+    glLightfv(GL_LIGHT0, GL_AMBIENT, amb0);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, diff0);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, spec0);
+
+    glLightfv(GL_LIGHT1, GL_AMBIENT, amb0);
+    glLightfv(GL_LIGHT1, GL_DIFFUSE, diff0);
+    glLightfv(GL_LIGHT1, GL_SPECULAR, spec0);
+
+    
     glMatrixMode(GL_PROJECTION); 
     glFrustum(-10,10,-10,10,0,1000);
     gluPerspective(90, 1, 1, 1000);
-
-    currentMode = solid;
-    currentShading = smooth;
-    lighting = true;
-    triangles = true;
-    currentMap = circles;
-    vertexShading = true;
 
 }
 
@@ -409,18 +274,151 @@ void display(void)
     glLoadIdentity();
     gluLookAt(eye[0],eye[1],eye[2], lookat[0],lookat[1],lookat[2], 0, 1, 0);
 	glLightfv(GL_LIGHT0, GL_POSITION, light1_pos);
-    glLightfv(GL_LIGHT0, GL_POSITION, light2_pos);
+    glLightfv(GL_LIGHT1, GL_POSITION, light2_pos);
     drawFloor();
+    drawBall();
     glutSwapBuffers();
     glutPostRedisplay();
-    //glutSetWindow(firstwindow);
-    
     
 }
 
 
 
+bool calcIntersection(vec3D r0, vec3D rd) {
+    //Max and min of bounding box for ball
+    vec3D min, max;
+    min.x = position[0] - 0.5f; min.y = position[1] - 0.5f; min.z = position[2] - 0.5f;
+    max.x = min.x + 1.0f; max.y = min.y + 1.0f; max.z = min.z + 1.0f;
+    
+    //Calculate points on the front face of the ball
+    point3D p0, p1, p2, p3;
+    p0.x = min.x; p0.y = min.y; p0.z = max.z;
+    p1.x = max.x; p1.y = min.y; p1.z = max.z;
+    p2.x = max.x; p2.y = max.y; p2.z = max.z;
+    p3.x = min.x; p3.y = max.y; p3.z = max.z;
+    
+    //Calculate the equation of the plane intersecting with the face
+    float a, b, c, d;
+    vec3D t1, t2, norm;
+    
+    t1 = t1.createVector(p1, p0);
+    t2 = t2.createVector(p2, p0);
+    
+    norm = t1.crossProduct(t2);
+    
+    norm = norm.normalize();
+    
+    d = -((norm.x * p0.x) + (norm.y * p0.y) + (norm.z * p0.z));
+    a = norm.x; b = norm.y; c = norm.z;
+    
+    //Test for intersection of the ray and the plane
+    float t, dot0, dotD;
+    vec3D is;
+    
+    dot0 = (a * r0.x) + (b * r0.y) + (c * r0.z);
+    dotD = (a * rd.x) + (b * rd.y) + (c * r0.z);
+    
+    if (dot0 == 0) return false;
+    
+    t = -((dot0 + d) / dotD);
+    
+    is = r0 + rd.vectorMultiply(t);
+    
+    if ((is.x > p0.x) && (is.x < p1.x) && (is.y > p0.y) && (is.y < p2.y)) {
+        return true;
+    } else {
+        return false;
+    }
+    
+    
+    
+}
 
+bool rayCast(float x, float y) {
+    GLint viewport[4];			//declaring arrays and variables
+    GLdouble modelview[16];
+    GLdouble projection[16];
+    float winX;
+    float winY;
+    GLdouble pos1X, pos1Y, pos1Z;
+    GLdouble pos2X, pos2Y, pos2Z;
+    GLdouble dirX, dirY, dirZ;
+    vec3D r0, rd;
+    
+    glGetIntegerv(GL_VIEWPORT, viewport);
+    glGetDoublev(GL_MODELVIEW_MATRIX, modelview);
+    
+    glGetDoublev(GL_PROJECTION_MATRIX, projection);
+    
+    //Flip the y coordinate to have the proper opengl screen coords
+    winX = x;
+    winY = (float)viewport[3] - y;
+    
+    //Unproject the coordinates from screen to world coordinates on the near clipping plane
+    gluUnProject(winX, winY, 0.0, modelview, projection, viewport, &pos1X, &pos1Y, &pos1Z);
+    
+    //Unproject the coordinates from screen to world coordinates on the far clipping plane
+    gluUnProject(winX, winY, 1.0, modelview, projection, viewport, &pos2X, &pos2Y, &pos2Z);
+    
+    //Calculate a normalized ray between the far and near clipping planes
+    dirX = pos2X - pos1X;
+    dirY = pos2Y - pos1Y;
+    dirZ = pos2Z - pos1Z;
+    
+    r0.x = pos1X; r0.y = pos1Y; r0.z = pos1Z;
+    rd.x = dirX; rd.y = dirY; rd.z = dirZ;
+    
+    rd = rd.normalize();
+    
+    if (calcIntersection(r0, rd)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+
+//void mouse(int btn, int state, int x, int y){
+//    if (btn == GLUT_LEFT_BUTTON && launched == false && selected == true){
+//        if (state == GLUT_DOWN){
+//            throwing = true;
+//            startingMousepos[0] = x, startingMousepos[1] = y;       //store the starting mouse position
+//            startTime=(GLfloat)glutGet(GLUT_ELAPSED_TIME);          //store the time when the user first clicks down
+//        }
+//        else if(state == GLUT_UP && throwing == true){
+//            
+//            finalMousepos[0] = x;
+//            finalMousepos[1] = y;
+//            endTime=(GLfloat)glutGet(GLUT_ELAPSED_TIME);          //store the time when the user first clicks down
+//            
+//            float timeElapsed = endTime - startTime;
+//            float dy = -(finalMousepos[1] - startingMousepos[1]);
+//            float dx = -(finalMousepos[0] - startingMousepos[0]);
+//            
+//            averageXVelocity = dx/timeElapsed;
+//            averageZVelocity = dy/timeElapsed;
+//                        velocity[0] += averageXVelocity*5;
+//                        velocity[1] = averageZVelocity*5;
+//                        velocity[2] += averageZVelocity*5;
+//            
+//            printf("%f,%f\n", averageXVelocity,averageZVelocity);
+//            launched = true;
+//            selected = false;
+//            throwing = false;
+//        }
+//    }
+//    //Ray casting for selecting the ball
+//    else if (btn == GLUT_LEFT_BUTTON && launched == false && selected == false) {
+//        if (state == GLUT_DOWN) {
+//            if (rayCast(float(x), float(y)) == true) {
+//                printf("Selected the object\n");
+//                selected = true;
+//            }
+//        }
+//    }
+//    
+//}
 
 void mouse(int btn, int state, int x, int y){
     
@@ -441,9 +439,9 @@ void mouse(int btn, int state, int x, int y){
             
             averageXVelocity = dx/timeElapsed;
             averageZVelocity = dy/timeElapsed;
-            velocity[0] += averageXVelocity*3;
-            velocity[1] = 10;
-            velocity[2] += averageZVelocity*3;
+            velocity[0] += averageXVelocity*5;
+            velocity[1] = averageZVelocity*5;
+            velocity[2] += averageZVelocity*5;
             
             printf("%f,%f\n", averageXVelocity,averageZVelocity);
             launched = true;

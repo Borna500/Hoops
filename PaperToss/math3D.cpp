3368cc25937d486246c359a4414727be2c9df1a8
Sean McKay <mckaysm@mcmaster.ca>
2016-12-08 16:08:40 -0500

    actually added apple support for math3d for realzies

--- PaperToss/math3D.cpp
+++ PaperToss/math3D.cpp
@@ -6,6 +6,16 @@ MacID: mckaysm, tranr5
 #include <math.h>
 #include "math3D.h"
 
+#ifdef __APPLE__
+#  include <OpenGL/gl.h>
+#  include <OpenGL/glu.h>
+#  include <GLUT/glut.h>
+#else
+#  include <GL/gl.h>
+#  include <GL/glu.h>
+#  include <GL/freeglut.h>	
+#endif
+
 
 point3D::point3D() {
 

e192870046637c2ea70cafecb135844c3ff7ce80
Neil Robichaud <neil.robichaud@hotmail.com>
2016-12-08 19:59:27 -0500

    textures yadda yadda

--- PaperToss/PaperToss.cpp
+++ PaperToss/PaperToss.cpp
@@ -24,13 +24,38 @@
 #include <cmath>
 #include <string>
 #include "math3D.h"
-#include "LevelLoader.h"
+//#include "LevelLoader.h"
 #include <string>
 #include <sstream>
 
 using namespace std;
-int firstwindow;
-int secondwindow;
+//brass
+float m_amb1[] = {0.135, 0.2225, 0.1575, 1.0}; //setting the material for the ambient, diffuse and specular values
+float m_diff1[] = {0.54, 0.89, 0.63, 1.0};
+float m_spec1[] = {0.316228, 0.316228, 0.316228, 1.0};
+float shiny1 = 0.1;
+
+//obsidian
+// float m_amb1[] = {0.05375, 0.05, 0.06625, 1.0}; //setting the material for the ambient, diffuse and specular values
+// float m_diff1[] = {0.18275, 0.17, 0.22525, 1.0};
+// float m_spec1[] = {0.332741, 0.328634, 0.346435, 1.0};
+// float shiny1 = 0.3;
+
+float m_amb2[] = {0.0, 0.0, 0.0, 1.0}; //setting the material for the ambient, diffuse and specular values black plastic
+float m_diff2[] = {0.01, 0.01, 0.01, 1.0};
+float m_spec2[] = {0.50, 0.50, 0.50, 1.0};
+float shiny2 = 0.25;
+
+float m_amb3[] = {0.0, 0.0, 0.0, 1.0}; //setting the material for the ambient, diffuse and specular values white plastic
+float m_diff3[] = {0.55, 0.55, 0.55, 1.0};
+float m_spec3[] = {0.70, 0.70, 0.70, 1.0};
+float shiny3 = 0.25;
+
+float m_amb4[] = {0.25, 0.20725, 0.20725, 1.0}; //setting the material for the ambient, diffuse and specular values pearl for ball
+float m_diff4[] = {1.00, 0.829, 0.829, 1.0};
+float m_spec4[] = {0.296648, 0.296648, 0.296648, 1.0};
+float shiny4 = 0.088;
+
 float amb0[4] = {1,1,1,1};
 float diff0[4] = {1,1,1,1};
 float spec0[4] = {1,1,1,1};
@@ -65,10 +90,11 @@ float basketPos[] = {0, basketheight, 4};
 float velocity[]  = {0,0,0};
 float acceleration[] = {windspeed,-100,0};
 
+float ballRotation[] = {0,0,0};
 /*** 
  EYE LOCATION
  ***/
-float eye[] = {0,1,-7};
+float eye[] = {0,0,0};
 float lookat[] = {0,0,0};
 float light1_pos[]= {0,4,-4,1}; //last num one is pofloat light
 float light2_pos[]= {0,0,0,1}; //last num one is pofloat light
@@ -84,39 +110,18 @@ int maxX = mapSize;
 int maxZ = mapSize;
 
 float eyex = 0;
-float eyey = 1;
+float eyey = 3;
 float eyez = -7;
 
 float lookatx = 0;
 float lookaty = 0;
+
 float lookatz = 0;
 
 
 bool spherical = false; 
 
-//self explanatory various states of the mesh
-enum mode{solid, wire, both};
-mode currentMode;
-
-enum shading{smooth, flat};
-shading currentShading;
-
-bool triangles;
-bool lighting;
-
-enum mapType {fault, circles};
-mapType currentMap;
-
-bool vertexShading;
-
-
-float minHeight = 0;
-float maxHeight = 0;
-
-
-// BORNA UPDATE FROM HERE
-
-float eyeradius = 7;
+float eyeradius = 10;
 float eyetheta = 4.71;
 float eyephi = 1.43;
 
@@ -140,17 +145,17 @@ int width, height, ppmax;  //width, height, max variables for the file of the te
 GLUquadricObj *sphereOBJ = NULL;
 
 void drawBall(){
-
-    
     glPushMatrix();
+    //everytime the ball travels one circumference it will rotate one revolution
     glTranslatef(position[0],position[1],position[2]);
-    //printf("%f",position[1]);
-    // glBindTexture(GL_TEXTURE_2D, textures[0]);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT,  m_amb4); //putting material onto the terrain
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE,  m_diff4);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR,  m_spec4);
+    glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS,  shiny4);
     sphereOBJ = gluNewQuadric();
     gluQuadricDrawStyle(sphereOBJ, GLU_FILL);
     gluQuadricTexture(sphereOBJ, GL_TRUE);
     gluQuadricNormals(sphereOBJ, GLU_SMOOTH);
-    
     gluSphere(sphereOBJ, 1, 100, 100);
     glPopMatrix();
 }
@@ -293,13 +298,6 @@ void cartesiontospherical(){
 
 
 
-// void updatelookatposition(){
-    
-//     lookat[0] = lookatradius * sin(lookatphi) * cos(lookattheta);
-//     lookat[1] = lookatradius  * cos(lookatphi);
-//     lookat[2] = lookatradius * sin(lookattheta) * sin(lookatphi);
-// }
-
 // BORNA UPDATE TO HERE
 void textprinter(int x, int y, char* text)
 {
@@ -310,20 +308,21 @@ void textprinter(int x, int y, char* text)
     glMatrixMode(GL_MODELVIEW);
     glPushMatrix();
     glLoadIdentity();
-    //glDisable(GL_DEPTH_TEST);
+    //glScalef(2,2,2);
     glRasterPos2i(x,y);
-
-    glColor3f(0,0,1);
+    
     char* c;  //character iterator (ptr)
     for(c = text; *c != '\0'; c++) //stop when we hit null character
     {
+
         glutBitmapCharacter(GLUT_BITMAP_9_BY_15, *c); //print one char
     }
+    
     glMatrixMode(GL_PROJECTION);
     glPopMatrix();
     glMatrixMode(GL_MODELVIEW);
+    
     glPopMatrix();
-//    glEnable(GL_DEPTH_TEST);
     
 }
 void changetostring(int integer){
@@ -338,6 +337,10 @@ void changetostring(int integer){
     
 }
 void drawFloor(){
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT,  m_amb1); //putting material onto the terrain
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE,  m_diff1);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR,  m_spec1);
+    glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS,  shiny1*128);
     //floor
     glPushMatrix();
         glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
@@ -413,7 +416,121 @@ void resetBall() {
     launched = false;
     bounced = false;
 }
+int snowmanCounter = 0;
+bool movingRight = true;
+float pos[] = {0,1,0};
+float rot[] = {0,0,0};
+float headRot[] = {0,0,0};
+void DrawSnowman(float* pos, float* rot){
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT,  m_amb3); //putting material onto the terrain
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE,  m_diff3);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR,  m_spec3);
+    glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS,  shiny3*128);
+    
+    glPushMatrix();
+    // printf("%i\n", counter);
+    if(movingRight == true){
+        if(snowmanCounter > -50){
+            glTranslatef(0.1*snowmanCounter,0,0);
+            snowmanCounter--;
+        } else if (snowmanCounter == -50){
+            glTranslatef(0.1*snowmanCounter,0,0);
+            snowmanCounter++;
+            movingRight = false;
+        }
+    }
 
+    if(movingRight == false){
+        if(snowmanCounter < 50){
+            glTranslatef(0.1*snowmanCounter,0,0);
+            snowmanCounter++;
+            
+        } else if(snowmanCounter == 50){
+            glTranslatef(0.1*snowmanCounter,0,0);
+            snowmanCounter--;
+            movingRight = true;
+        }
+    }
+
+    
+    glPushMatrix();
+    
+    glTranslatef(pos[0], pos[1], pos[2]);
+    // glRotatef(rot[1], 0, 1, 0);
+    glRotatef(180,0,1,0);
+    
+    //draw body
+    glColor3f(1,1,1);
+    glutSolidSphere(1, 16, 16);
+    
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT,  m_amb2); //putting material onto the terrain
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE,  m_diff2);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR,  m_spec2);
+    glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS,  shiny2*128);
+    
+    //draw buttons
+    glPushMatrix();
+    glTranslatef(0, 0.35, 0.9);
+    glColor3f(0, 0, 0);
+    glutSolidSphere(0.1, 10, 10);
+    glPopMatrix();
+    
+    glPushMatrix();
+    glTranslatef(0, 0.15, 0.95);
+    glColor3f(0, 0, 0);
+    glutSolidSphere(0.1, 10, 10);
+    glPopMatrix();
+    
+    glPushMatrix();
+    glTranslatef(0, -0.05, 0.95);
+    glColor3f(0, 0, 0);
+    glutSolidSphere(0.1, 10, 10);
+    glPopMatrix();
+    
+    
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT,  m_amb3); //putting material onto the
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE,  m_diff3);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR,  m_spec3);
+    glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS,  shiny3*128);
+    
+    glPushMatrix();
+    //translate relative to body, and draw head
+    glTranslatef(0, 1.25, 0);
+    glRotatef(headRot[1], 0, 1, 0); //turn the head relative to the body
+    glColor3f(1,1,1);
+    glutSolidSphere(0.5, 16, 16);
+    
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT,  m_amb2); //putting material onto the
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE,  m_diff2);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR,  m_spec2);
+    glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS,  shiny2*128);
+    
+    //translate and draw right eye
+    glPushMatrix();
+    glTranslatef(0.2, 0.15, 0.45);
+    glColor3f(0,0,0);
+    glutSolidSphere(0.1, 10, 10);
+    glPopMatrix();
+    
+    //translate and draw left eye
+    glPushMatrix();
+    glTranslatef(-0.2, 0.15, 0.45);
+    glColor3f(0,0,0);
+    glutSolidSphere(0.1, 10, 10);
+    glPopMatrix();
+    
+    //translate and draw nose
+    glPushMatrix();
+    glTranslatef(0, 0, 0.5);
+    glColor3f(1,0.4,0);
+    glutSolidSphere(0.1, 10, 10);
+    glPopMatrix();
+    
+    glPopMatrix();//body
+    glPopMatrix();//snowman
+    
+    glPopMatrix();
+}
 void drawBasket() {
     glColor3f(1,0,0);
     glPushMatrix();
@@ -445,8 +562,10 @@ void ballMotion(int value){
         }
         else{
             currentTime = (GLfloat)glutGet(GLUT_ELAPSED_TIME);
-            printf("%f\n", currentTime-groundstartTime);
-            if (currentTime - groundstartTime > 5000){
+           // printf("%f\n", currentTime-groundstartTime);
+            if ((currentTime - groundstartTime) > 5000){
+                printf("times up\n");
+                
                 pendingStop = false;
                 launched = false;
                 resetBall();
@@ -460,6 +579,7 @@ void ballMotion(int value){
 	    position[0] += velocity[0]/60;
 	    position[1] += velocity[1]/60;
 	    position[2] += velocity[2]/60;
+
 
     
         if (position[2] > 5 ){
@@ -478,6 +598,7 @@ void ballMotion(int value){
             printf("out of x neg wall");
         }
         else if (position[0] > 5){
+            
             velocity[0] = -1*(velocity[0] - 0.5*velocity[0]);
             position[0] = 5.0 + velocity[0]/60;
             printf("out of x pos wall");
@@ -492,6 +613,7 @@ void ballMotion(int value){
         if ((position[0] > (basketPos[0] - basketRadius)) && (position[0] < (basketPos[0] + basketRadius))) {
             if ((position[2] > (basketPos[2] - basketRadius)) && (position[2] < (basketPos[2] + basketRadius))) {
                 if (position[1] > basketPos[1] && position[1] < (basketPos[1] + 0.5f) && !bounced) {
+                    printf("scored\n");
                     resetBall();
                     intscorecounter++;
                 }
@@ -517,7 +639,7 @@ void ballMotion(int value){
 }
 
 float input1, input2, input3;
-
+int text = 2;
 void keyboard(unsigned char key, int x, int y)
 {
 
@@ -531,22 +653,46 @@ void keyboard(unsigned char key, int x, int y)
             
 		case 'W':
         case 'w':
-      	eyez +=0.1;
+            if (eyez < 0){
+                eyez +=0.1;
+
+            }
+            else{
+                eyez -=0.1;
+            }
         	break;
 
         case 'a':
         case 'A':
-          	eyex +=0.1;
+            if (eyez < 0){
+                eyex +=0.1;
+                
+            }
+            else{
+                eyex -=0.1;
+            }
         	break;
 
     	case 'S':
         case 's':
-        	eyez -=0.1;
+            if (eyez < 0){
+                eyez -=0.1;
+                
+            }
+            else{
+                eyez +=0.1;
+            }
         	break;
 
         case 'D':
         case 'd':
-          	eyex -=0.1;
+            if (eyez < 0){
+                eyex -=0.1;
+                
+            }
+            else{
+                eyex +=0.1;
+            }
         	break;
 
         case 'U':
@@ -608,6 +754,8 @@ void keyboard(unsigned char key, int x, int y)
         case '1':
             if (textureToggle == true){
                 glEnable(GL_TEXTURE_2D);
+                // glBindTexture(GL_TEXTURE_2D, textures[0]);
+                
                 textureToggle = false;
             } else {
                 glDisable(GL_TEXTURE_2D);
@@ -616,18 +764,20 @@ void keyboard(unsigned char key, int x, int y)
             break;
             
         case '2':
-            glBindTexture(GL_TEXTURE_2D, textures[0]);
-            // drawBall();
+            text = 2;
+            // glBindTexture(GL_TEXTURE_2D, textures[0]);
             break;
             
         case '3':
-            glBindTexture(GL_TEXTURE_2D, textures[1]);
-            // drawBall();
+            text = 3;
+            // glBindTexture(GL_TEXTURE_2D, textures[1]);
+            
             break;
             
         case '4':
-            glBindTexture(GL_TEXTURE_2D, textures[2]);
-            // drawBall();
+            text = 4;
+            // glBindTexture(GL_TEXTURE_2D, textures[2]);
+            
             break;
             
         case 'r':
@@ -671,7 +821,7 @@ void init(void)
     
     glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0);
-    glEnable(GL_LIGHT1);
+    //glEnable(GL_LIGHT1);
     glLightfv(GL_LIGHT0, GL_AMBIENT, amb0);
     glLightfv(GL_LIGHT0, GL_DIFFUSE, diff0);
     glLightfv(GL_LIGHT0, GL_SPECULAR, spec0);
@@ -689,7 +839,21 @@ void init(void)
 }
 
 
-
+void ballTexture(){
+    switch(text){
+        case 2:
+            glBindTexture(GL_TEXTURE_2D, textures[0]);
+            break;
+            
+        case 3:
+            glBindTexture(GL_TEXTURE_2D, textures[1]);
+            break;
+            
+        case 4:
+            glBindTexture(GL_TEXTURE_2D, textures[2]);
+            break;
+    }
+}
 
 void display(void)
 {
@@ -700,18 +864,21 @@ void display(void)
     glLoadIdentity();
     changetostring(intscorecounter);
     glDisable(GL_LIGHTING);
+    glColor3f(1, 0, 0);
     textprinter(750,780, (char*)counterstring);
     glEnable(GL_LIGHTING);
     gluLookAt(eye[0],eye[1],eye[2], lookat[0],lookat[1],lookat[2], 0, 1, 0);
     glLightfv(GL_LIGHT0, GL_POSITION, light1_pos);
     glLightfv(GL_LIGHT1, GL_POSITION, light2_pos);
     
-    glDisable(GL_TEXTURE_2D);
     drawFloor();
+    glDisable(GL_TEXTURE_2D);
     drawBasket();
+    DrawSnowman(pos, rot);
     if(textureToggle == true){
         glEnable(GL_TEXTURE_2D);
     }
+    ballTexture();
     drawBall();
     glutSwapBuffers();
     glutPostRedisplay();
@@ -837,7 +1004,7 @@ void mouse(int btn, int state, int x, int y){
             velocity[1] = 20;
             velocity[2] += averageZVelocity*5;
             
-            printf("%f,%f\n", averageXVelocity,averageZVelocity);
+           // printf("%f,%f\n", averageXVelocity,averageZVelocity);
             launched = true;
             bounced = false;
         }
@@ -860,7 +1027,7 @@ int main(int argc, char** argv)
     glutInitWindowSize(800, 800);
     glutInitWindowPosition(100, 100);
 
-    firstwindow = glutCreateWindow("BallToss");  //creates the window
+    glutCreateWindow("BallToss");  //creates the window
     
     
     glutDisplayFunc(display);   //registers "display" as the display callback function
@@ -869,9 +1036,6 @@ int main(int argc, char** argv)
     glutSpecialFunc(special);
     
     glEnable(GL_DEPTH_TEST);
-    //glFrontFace((GL_CCW));
-    //glCullFace(GL_FRONT);
-    //glEnable(GL_CULL_FACE);
     init();
     initTexture();
     callbackinit();
